5e6d3db2a72c33604a46354fa6dc6a909d3fe8af
sriram A <sriram.coder2004@gmail.com>
2025-05-30 15:34:53 +0530

    Initial commit

---
+++ js/new.js
@@ -0,0 +1,4 @@
+document.getElementById("back-to-top").addEventListener("click", function() {
+    // Scroll to the top of the page smoothly
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  });
